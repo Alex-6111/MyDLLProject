8a4cf3bad9a517322c0444556eb268743f129e2d
Admin <obekas777@gmail.com>
2022-08-23 23:22:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using  MyLibrary;
+
+namespace MyDLLProject
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            double result = MyDLL.Multiply(3,5);
+            Console.WriteLine(result);
+        }
+    }
+}
